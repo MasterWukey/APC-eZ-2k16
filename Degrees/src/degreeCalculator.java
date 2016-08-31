1a518f2cb4c37cde04c82a660518677111c59677
MasterWukey <Klukeykukey@yahoo.com>
2016-08-31 10:04:16 -0700

    degree calculator

---
+++ Degrees/src/degreeCalculator.java
@@ -0,0 +1,8 @@
+
+public class degreeCalculator {					//create class
+	public static void main(String[]args){		//create method
+		double f=68;							//declare and assign farenheit
+		double c=(f-32)*5/9;					//declare and assign celcius
+		System.out.print("The degrees celcius is: " + c);
+	}
+}
