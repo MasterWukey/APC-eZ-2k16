13c0f685df9d0a387f5d79381262e1471c30bdb4
MasterWukey <Klukeykukey@yahoo.com>
2016-09-30 09:08:33 -0700

    cumAlgorithim day 2

--- cumlative algorithim/src/CuumAlgorithim.java
+++ cumlative algorithim/src/CuumAlgorithim.java
@@ -1,6 +1,12 @@
-import.java.util*;
+import java.util.*;
 public class CuumAlgorithim {
 
-	public static void main
+	public static void main(String[]args){
+		Scanner console=new Scanner(System.in);
+		
+		int min;
+		int max;
+		int totalSum;
+	}
 	
 }
