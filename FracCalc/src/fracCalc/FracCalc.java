ee251b10e55995b0da78ffc40b413488320de7fb
MasterWukey <Klukeykukey@yahoo.com>
2016-12-01 09:46:44 -0800

    frac calc checkpoint 1 done

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -3,32 +3,56 @@ import java.util.*;
 
 public class FracCalc {
 
-    public static void main(String[] args) 
+   /* public static void main(String[] args) 		//original checkpoint ONE work
     {
         Scanner console= new Scanner(System.in);
         System.out.println("enter expression");
         String expression= console.nextLine();
-    	produceAnswer(expression);
+    	System.out.println(produceAnswer(expression));
 
+    }
+    
+    public static String produceAnswer(String input)
+    { 
+       for(int i=0; i<input.length(); i++){
+    	   if(i==input.indexOf(" ")){
+    	   int opIndex=i;
+    	   String Op1= input.substring(0, i);
+    	   String Op2= input.substring(i+3, input.length());
+    	   String operator= input.substring(i, i+1);
+    	   return Op2;
+    	   }  
+    	   
+       }
+	return null;
+    
+    }						//END of checkpoint ONE code
+  */  
+	public static void main(String[] args) 		//checkpoint TWO code
+    {
+        Scanner console= new Scanner(System.in);
+        System.out.println("enter expression");
+        String expression= console.nextLine();
+        while (expression.indexOf("quit")<0){
+    	System.out.println(produceAnswer(expression));
+        }
     }
     
    
     public static String produceAnswer(String input)
     { 
        for(int i=0; i<input.length(); i++){
-    	   if(i==input.indexOf("+")){
-    		   
-    	   int opIndex= input.indexOf("+");
-    	   String Op1= input.substring(0, opIndex);
-    	   String Op2= input.substring(opIndex, input.length());
-    		   
-    	   }
-       
-        
-        return " ";
-    }
-
-    
+    	   if(i==input.indexOf(" ")){
+    	   int opIndex=i;
+    	   String Op1= input.substring(0, i);
+    	   String Op2= input.substring(i+3, input.length());
+    	   String operator= input.substring(i, i+1);
+    	   return Op2;
+    	   }  
+    	   
+       }
+	return null;
     
     }
-}
+    
+}//end of class bracket-KEEP
